fd599a17ea04dd10e7f397b0bde6e4f823e272ec
pavpench <p.penchev90@gmail.com>
2023-07-15 10:03:57 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct secretNumber';
+// document.querySelector('.secretNumber').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// console.log(document.querySelector('.guess').value);
